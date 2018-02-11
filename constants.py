49c96dac7d6d1b87d78ba545091fe62b2f555fa2
devndra <horrormyth@gmail.com>
2018-02-11 13:00:41 +0200

    Update constants

--- constants.py
+++ constants.py
@@ -1,7 +1,34 @@
+import os
+
+from nordea.generate_access_token import generate_access_token
+
 OP = 'OP'
 NORDEA = 'Nordea'
 
-NORDEA_HEADERS = ''
-OP_HEADERS = ''
-OP_BASE_URL = ''
-NORDEA_BASE_URL = ''
+nordea_access_token = generate_access_token()
+# endpoint we query
+endpoint = 'v2/accounts'
+# set headers and make the request
+
+NORDEA_BASE_URL = 'https://api.nordeaopenbanking.com/'
+NORDEA_CLIENT_ID = os.environ(['NORDEA_CLIENT_ID'])
+NORDEA_CLIENT_SECRET = os.environ(['NORDEA_SECRET'])
+
+OP_BASE_URL = "https://sandbox.apis.op-palvelut.fi/"
+OP_CLIENT_ID = os.environ(['OP_CLIENT_ID'])
+OP_CLIENT_SECRET = os.environ(['OP_SECRET'])
+
+NORDEA_HEADERS = {
+    'X-IBM-Client-Id': NORDEA_CLIENT_ID,
+    'X-IBM-Client-Secret': NORDEA_CLIENT_SECRET,
+    'Authorization': 'Bearer {}'.format(nordea_access_token),
+    'content-type': 'application/json'
+
+}
+OP_HEADERS = {
+    'x-api-key': os.environ.get('X_API_KEY'),
+    'x-request-id': "12345",
+    'x-session-id': "12345",
+    'x-authorization': "fdb6c7c24bbc3a2c4144c1848825ab7d3a4ccb43",
+    'content-type': 'application/json'
+}
